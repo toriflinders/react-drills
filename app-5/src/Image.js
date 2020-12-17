18995d5dffc190c17d967844ded4ce9953dcd816
toriflinders <toriflinders04@gmail.com>
2020-12-17 13:47:46 -0700

    "first commit"

---
+++ app-5/src/Image.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export default class Image extends Component {
+  render(){
+    return (
+      <div>
+        <img src={this.props.url} />
+        <div>Her Last Performance</div>
+      </div>
+    )
+  }
+}
+
